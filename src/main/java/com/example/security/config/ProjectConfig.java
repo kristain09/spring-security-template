b03abf081d3be7462671c0c1ba8773a338342fd8
Kristain Putra <kputrapar@gmail.com>
2023-11-16 23:20:24 +0700

    adding configure bean method with body as same with default

--- src/main/java/com/example/security/config/ProjectConfig.java
+++ src/main/java/com/example/security/config/ProjectConfig.java
@@ -3,12 +3,15 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 
 @Configuration
@@ -21,6 +24,12 @@ public class ProjectConfig {
         return new InMemoryUserDetailsManager(kris);
     }
 
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+        return http.build();
+    }
 
     @Bean
     public PasswordEncoder passwordEncoder() {
