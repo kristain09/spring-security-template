9301fd10f7b637acaa767802589d1543ea3a5864
Kristain Putra <kputrapar@gmail.com>
2023-11-16 23:11:35 +0700

    add user to inmemoryuserdetailservice

--- src/main/java/com/example/security/config/ProjectConfig.java
+++ src/main/java/com/example/security/config/ProjectConfig.java
@@ -3,7 +3,11 @@ package com.example.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 
@@ -12,7 +16,15 @@ public class ProjectConfig {
 
     @Bean
     UserDetailsService userDetailsService() {
-        return new InMemoryUserDetailsManager();
+        UserDetails kris =
+                User.withUsername("kris").password("password").authorities("read").build();
+        return new InMemoryUserDetailsManager(kris);
     }
 
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        //deprecated but i do not care
+        return NoOpPasswordEncoder.getInstance();
+    }
 }
